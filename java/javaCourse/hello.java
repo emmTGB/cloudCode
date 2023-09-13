ffc50afc13671e92f4ad25b05522c53cef5df9d8
emmTGB <lsg3079425053@gmail.com>
2023-09-13 09:48:18 +0800

    update

--- java/javaCourse/hello.java
+++ java/javaCourse/hello.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class hello{
     public static void main(String args[]){
         System.out.print("hello");
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.nextInt());
+        in.close();
     }
 }
