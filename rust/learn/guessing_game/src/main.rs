b35105c9266f71873d683b686416f3f120c7c39c
emmtgb <lsg3079425053@gmail.com>
2023-09-25 20:54:40 +0800

    kali vsc editted

--- rust/learn/guessing_game/src/main.rs
+++ rust/learn/guessing_game/src/main.rs
@@ -1,14 +1,36 @@
 //use std::io;  // prelude
+use rand::Rng;  // trait
+use std::cmp::Ordering;
 
 fn main(){
-    println!("guess one");
-
+    
     // let foo = 1;
-    // let bar = foo;  // immutable
+    // let bar = foo;  // default immutable
+    
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("secret_number is: {}", secret_number);
 
-    let mut guess = String::new();
+    loop{
+        println!("guess one");
 
-    std::io::stdin().read_line(&mut guess).expect("cannot read");
+        let mut guess = String::new();
+        std::io::stdin().read_line(&mut guess).expect("cannot read");
+        // io::Result   Ok Err
 
-    println!("you guessed: {}", guess);
+        let guess:u32 = match guess.trim()/* delete spaces */.parse()/* transfer to other type */{
+            Ok(num) => num,
+            Err(_) => continue,
+        };  // shadow
+
+        println!("you guessed: {}", guess);
+
+        match guess.cmp(&secret_number){  // arm
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("you win");
+                break;
+            },
+        }
+    }
 }
