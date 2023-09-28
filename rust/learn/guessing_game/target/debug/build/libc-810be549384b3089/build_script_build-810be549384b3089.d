f7ddeb5a620b8f8fb058c3f78abe708e49ec8a00
emmtgb <lsg3079425053@gmail.com>
2023-09-28 10:27:05 +0800

    kali vsc editted

--- rust/learn/guessing_game/target/debug/build/libc-810be549384b3089/build_script_build-810be549384b3089.d
+++
@@ -1,5 +0,0 @@
-/home/byeol/code/cloudCode/rust/learn/guessing_game/target/debug/build/libc-810be549384b3089/build_script_build-810be549384b3089: /home/byeol/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.148/build.rs
-
-/home/byeol/code/cloudCode/rust/learn/guessing_game/target/debug/build/libc-810be549384b3089/build_script_build-810be549384b3089.d: /home/byeol/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.148/build.rs
-
-/home/byeol/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.148/build.rs:
--- rust/learn/guessing_game/target/debug/deps/guessing_game-d1237cbe13a633ad.d
+++
@@ -1,5 +0,0 @@
-/home/byeol/code/cloudCode/rust/learn/guessing_game/target/debug/deps/guessing_game-d1237cbe13a633ad.rmeta: src/main.rs
-
-/home/byeol/code/cloudCode/rust/learn/guessing_game/target/debug/deps/guessing_game-d1237cbe13a633ad.d: src/main.rs
-
-src/main.rs:
