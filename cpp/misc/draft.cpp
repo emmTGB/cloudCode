641538b47fd38281484a380b7771276091783ec2
emmTGB <lsg3079425053@outlook.com>
2023-10-03 22:17:45 +0800

    Merge branch 'main' of github.com:/emmTGB/cloudCode

--- cpp/misc/draft.cpp
+++ cpp/misc/draft.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
+#include<hash_fun.h>
+#include<string>
 using namespace std;
 
 int main(){
+    double dec = 3.14159265;
+    size_t hashStr = hash<double>()(dec);
+    cout<<hashStr;
     cout<<"看得到吗"<<endl;
 }
