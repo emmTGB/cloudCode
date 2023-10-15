f0f563956e8893feb78140502e9991f446959880
emmTGB <lsg3079425053@outlook.com>
2023-10-15 15:14:30 +0800

    win vsc editted

--- rust/learn/path/src/lib.rs
+++ rust/learn/path/src/lib.rs
@@ -5,7 +5,40 @@ pub mod front_of_house{
 }
 
 pub fn eat_at_restuarant(){
-    crate::front_of_house::hosting::add_to_waitlist();
+    crate::front_of_house::hosting::add_to_waitlist();  // 绝对路径
 
-    front_of_house::hosting::add_to_waitlist();
+    front_of_house::hosting::add_to_waitlist();  // 相对路径
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("like {} toast", meal.toast);
+    // meal.seasonal_fruit xx
 }
+
+fn serve_orde(){}
+
+mod back_of_house{
+    pub struct Breakfast{
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast{
+        pub fn summer(toast: &str)->Breakfast{
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order(){
+        cook_order();
+        super::serve_orde();
+        crate::serve_orde();
+    }
+
+    fn cook_order(){}
+}
+
+mod mid_of_house;
--- rust/learn/path/src/main.rs
+++ rust/learn/path/src/main.rs
@@ -1,3 +1,11 @@
+use path::*;
+use std::collections::hash_map as map;
+pub use path::front_of_house::hosting;
+
+use rand::Rng;
+
+use std::{cmp::Ordering, io::{self, Write}};
+
 fn main() {
     println!("Hello, world!");
 }
