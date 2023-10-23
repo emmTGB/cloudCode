30cd96380ba046617e14ac0d4c77167c45149a1b
emmTGB <lsg3079425053@gmail.com>
2023-10-23 22:26:09 +0800

    win vsc edited

--- cpp/cf/r905d3/D.cpp
+++ cpp/cf/r905d3/D.cpp
@@ -1,32 +1,41 @@
 //
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define pii pair<int, int>
 
-void solve(){
+void solve()
+{
     multiset<pii> x, y;
     int n, p, q;
     char ch;
-    cin>>n;
-    for(int i = 0; i < n; i++){
-        cin>>ch>>p>>q;
-        if(ch == '+'){
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ch >> p >> q;
+        if (ch == '+')
+        {
             x.insert({p, q});
             y.insert({q, p});
-        }else{
+        }
+        else
+        {
             x.erase(x.find({p, q}));
             y.erase(y.find({q, p}));
         }
 
-        if(x.size() && x.rbegin()->first > y.begin()->first){
-            cout<<"YES";
-        }else{
-            cout<<"NO";
+        if (x.size() && x.rbegin()->first > y.begin()->first)
+        {
+            cout << "YES";
         }
-        cout<<endl;
+        else
+        {
+            cout << "NO";
+        }
+        cout << endl;
     }
 }
 
-int main(){
+int main()
+{
     solve();
 }
--- cpp/cf/r905d3/E.cpp
+++ cpp/cf/r905d3/E.cpp
@@ -1,28 +1,32 @@
 //
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 
-
-void solve(){
+void solve()
+{
     ll res, m, n, p, q;
-    cin>>m;
-    for(ll i = 0; i < m; i++){
-        cin>>n;
+    cin >> m;
+    for (ll i = 0; i < m; i++)
+    {
+        cin >> n;
         res = 0;
         p = 0;
-        for(ll j = 0; j < n; j++){
-            cin>>q;
-            while(q < p){
+        for (ll j = 0; j < n; j++)
+        {
+            cin >> q;
+            while (q < p)
+            {
                 q *= 2;
                 ++res;
             }
             p = q;
         }
-        cout<<res;
+        cout << res << endl;
     }
 }
 
-int main(){
+int main()
+{
     solve();
 }
