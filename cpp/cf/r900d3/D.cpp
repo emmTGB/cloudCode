76e71595e772c0f074d580150dd89002e499de90
emmTGB <lsg3079425053@outlook.com>
2023-10-24 19:28:58 +0800

    debian vsc edited

--- cpp/cf/r900d3/D.cpp
+++ cpp/cf/r900d3/D.cpp
@@ -4,6 +4,16 @@ using namespace std;
 #define mii map<int, int>
 typedef long long ll;
 
+inline string read()
+{
+    char ch = getchar();
+    string st1 = "";
+    while (!((ch >= 'a') && (ch <= 'z')))
+        ch = getchar();
+    while ((ch >= 'a') && (ch <= 'z'))
+        st1 += ch, ch = getchar();
+    return st1;
+}
 void read(ll& x) {
     int s = 0, w = 1;
     char ch = getchar();
@@ -20,21 +30,6 @@ void read(ll& x) {
     x = s * w;
 }
 
-ll read() {
-    int s = 0, w = 1;
-    char ch = getchar();
-    while (ch < '0' || ch > '9') {
-        if (ch == '-') {
-            w = -1;
-            ch = getchar();
-        }
-    }
-    while (ch >= '0' && ch <= '9') {
-        s = s * 10 + ch - '0';
-        ch = getchar();
-    }
-    return 1ll * s * w;
-}
 
 void _print(ll x) {
     if (x > 9)_print(x / 10);
@@ -75,16 +70,71 @@ ll qp(ll x, ll p)
     return ans;
 }
 
-void solve() {
-    ll n, k, x;
-    cin >> n >> k >> x;
+struct Node {
+    ll l, r;
+};
 
-    if (x < 1l * (1 + k) * k / 2 || x > 1l * (2 * n - k + 1) * k / 2) {
-        cout << "NO" << endl;
+void solve() {
+    set<ll> x;
+    string s;
+    ll n, k, q, tmp;
+    cin >> n >> k;
+    s = read();
+    vector<Node*> ran(k);
+    for (int i = 0; i < k;i++)ran[i] = new Node;
+    for (int i = 0; i < k; i++) {
+        scanf("%lld", &tmp);
+        ran[i]->l = tmp - 1;
     }
-    else {
-        cout << "YES" << endl;
+    for (int i = 0; i < k; i++) {
+        scanf("%lld", &tmp);
+        ran[i]->r = tmp - 1;
     }
+    cin >> q;
+    for (ll i = 0; i < q; i++) {
+        scanf("%lld", &tmp);
+        if (x.count(tmp - 1)) {
+            x.erase(tmp - 1);
+        }
+        else {
+            x.insert(tmp - 1);
+        }
+    }
+    vector<int> flag(n);
+    for (auto it = x.begin(); it != x.end(); it++) {
+        tmp = *it;
+        ll j = k / 2;
+        ll L = -1, R = k;
+        while (j > -1 && j < k) {
+            if (ran[j]->l > tmp) {
+                R = j;
+                j = (L + R) / 2;
+            }
+            else if (ran[j]->r < tmp) {
+                L = j;
+                j = (L + R) / 2;
+            }
+            else {
+                break;
+            }
+        }
+        ll a = min(tmp, ran[j]->l + ran[j]->r - tmp);
+        ll b = max(tmp, ran[j]->l + ran[j]->r - tmp);
+        ++flag[a], --flag[b];
+    }
+    vector<int> sol(n);
+    int now = 0;
+    for (int i = 0; i < n; i++) {
+        now += flag[i];
+        sol[i] = now % 2;
+    }
+    for (int i = 0; i < k; i++) {
+        for (int j = ran[i]->l; 2 * j <= ran[i]->l + ran[i]->r; j++) {
+            if (sol[j] != 0)
+                swap(s[j], s[ran[i]->l + ran[i]->r - j]);
+        }
+    }
+    cout << s << endl;
 }
 
 int main() {
@@ -93,5 +143,4 @@ int main() {
     while (t--) {
         solve();
     }
-    return 0;
 }
