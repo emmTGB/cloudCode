49b062e5906626912c664bfba7f137955a8c2da7
emmTGB <lsg3079425053@gmail.com>
2023-11-28 22:04:38 +0800

    java dos final

--- java/JavaExperiences/FileManagementSys/src/Process/Doc.java
+++ java/JavaExperiences/FileManagementSys/src/Process/Doc.java
@@ -9,7 +9,7 @@ public class Doc {
     private String description;
     private String filename;
 
-    Doc(String ID,String creator,long timestamp,String description, String filename){
+    Doc(String ID, String creator, long timestamp, String description, String filename) {
         super();
         this.ID = ID;
         this.creator = creator;
@@ -17,49 +17,50 @@ public class Doc {
         this.description = description;
         this.filename = filename;
     }
-    public String getID(){
+
+    public String getID() {
         return ID;
     }
 
-    public String getCreator(){
+    public String getCreator() {
         return creator;
     }
 
-    public long getTimestamp(){
+    public long getTimestamp() {
         Date date = new Date();
         return date.getTime();
     }
 
-    public String getDescription(){
+    public String getDescription() {
         return description;
     }
 
-    public String getFilename(){
+    public String getFilename() {
         return filename;
     }
 
-    public void setID(String id){
+    public void setID(String id) {
         ID = id;
     }
 
-    public void setCreator(String Creator){
+    public void setCreator(String Creator) {
         creator = Creator;
     }
 
-    public void setTimestamp(long Timestamp){
+    public void setTimestamp(long Timestamp) {
         timestamp = Timestamp;
     }
 
-    public void setDescription(String Description){
+    public void setDescription(String Description) {
         description = Description;
     }
 
-    public void setFilename(String Filename){
+    public void setFilename(String Filename) {
         filename = Filename;
     }
 
     @Override
-    public String toString(){
-        return ID + "\n" + use
+    public String toString() {
+        return ID + "\n" + creator + "\n" + timestamp + "\n" + description + "\n" + filename + "\n";
     }
 }
