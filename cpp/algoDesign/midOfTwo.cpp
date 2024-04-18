a7d9122b27011cfc3a82b90c0bc17798e76dd025
emmTGB <lsg3079425053@outlook.com>
2024-04-18 12:44:36 +0800

    1

--- cpp/algoDesign/midOfTwo.cpp
+++ cpp/algoDesign/midOfTwo.cpp
@@ -1,30 +1,47 @@
 #include<iostream>
 using namespace std;
 
-double mid(int a[], int b[], int n) {
-    int j = n - 1, k = 0;
-    if (a[j] <= b[k]) {
-        return (a[n - 1] + b[0]) / 2.0;
-    }
-    while (a[j] > b[k]) {
+int* a, * b;
 
+double find_k(int l1, int r1, int l2, int r2, int k) {
+    int cur_1 = r1 - l1 + 1;
+    int cur_2 = r2 - l2 + 1;
+    if (k == 1) {
+        return min(a[l1], b[l2]);
     }
-    return 0;
+    if (cur_1 == 0) {
+        return b[l2 + k - 1];
+    }
+    if (cur_2 == 0) {
+        return a[l1 + k - 1];
+    }
+    int m1 = l1 + min(cur_1, k / 2) - 1;
+    int m2 = l2 + min(cur_2, k / 2) - 1;
+    if (a[m1] <= b[m2]) {
+        return find_k(m1 + 1, r1, l2, m2, k - (m1 - l1 + 1));
+    }
+    else {
+        return find_k(l1, r1, m2 + 1, r2, k - (m2 - l2 + 1));
+    }
+}
+
+double mid(int n) {
+    int n1 = find_k(0, n - 1, 0, n - 1, n);
+    int n2 = find_k(0, n - 1, 0, n - 1, n + 1);
+    return (n1 + n2) / 2.0;
 }
 
 
 int main() {
     int n;
     cin >> n;
-    int* a = new int[n];
-    int* b = new int[n];
+    a = new int[n];
+    b = new int[n];
     for (int i = 0;i < n;i++) {
         cin >> a[i];
     }
     for (int i = 0;i < n;i++) {
         cin >> b[i];
     }
-    if (a[n - 1] < b[n - 1]) {
-        swap(a, b);
-    }
+    cout << mid(n) << endl;
 }
